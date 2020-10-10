9b680ada20c95e5263b5e2b8422c1334cc63fdec
Renki27 <emilio.rojas@ucrso.info>
2020-10-10 14:41:55 -0600

    Create loadQuestions.php

---
+++ loadQuestions.php
@@ -0,0 +1,64 @@
+<?php
+
+
+require 'DB.php';
+
+if ($_GET["request"] == "loadScrQue") {
+
+
+  //  echo "GET";
+
+    $questionsPayload = loadScratchQuestions($database);
+
+    if ($questionsPayload) {
+        $length = count($questionsPayload);
+        $scratchQuestions = [];
+        if($length > 0) {
+            for ($i = 0; $i < $length; $i++) {
+
+            
+                $questions =
+                    array(
+                        "question_text" => $questionsPayload[$i]["QUESTION_TEXT"],
+                        "question_info" => $questionsPayload[$i]["QUESTION_INFORMATION"],
+                        "question_img" => $questionsPayload[$i]["QUESTION_IMAGE"],
+                        "option1" => $questionsPayload[$i]["T3_OPTION_Q_1"],
+                        "option2" => $questionsPayload[$i]["T3_OPTION_Q_2"],
+                        "option3" => $questionsPayload[$i]["T3_OPTION_Q_3"],
+                        "option4" => $questionsPayload[$i]["T3_OPTION_Q_4"],
+                        "answer" => $questionsPayload[$i]["T3_ANSWER"]
+                    );
+                    array_push($scratchQuestions, $questions); 
+            }
+
+        }
+  
+
+
+        $response['status'] = "Success";
+        $response['message'] = "Questions loaded loaded!";
+        $response['scratchQuestions'] = $scratchQuestions;
+    } else {
+        $response['status'] = "Error";
+        $response['message'] = "Questions not found!"; 
+    }
+
+
+
+
+
+
+
+  //  header("Content-type: application/json");
+    echo json_encode($response);
+}
+
+
+
+function loadScratchQuestions($database)
+{
+    $sql = 'CALL `SELECT_SCRATCH_QUESTIONS`()';
+    $sth = $database->pdo->prepare($sql);
+    $sth->execute();
+    return $sth->fetchAll(PDO::FETCH_SERIALIZE);
+}
