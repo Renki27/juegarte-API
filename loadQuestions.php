9183b49ac65b9a5da6d91d9e5c0c5115a6ea31cc
rebecaperaza <66180590+rebecaperaza@users.noreply.github.com>
2020-10-14 10:49:14 -0600

    loadTrueFalseQuestions

--- loadQuestions.php
+++ loadQuestions.php
@@ -62,3 +62,60 @@ function loadScratchQuestions($database)
     $sth->execute();
     return $sth->fetchAll(PDO::FETCH_SERIALIZE);
 }
+
+
+if ($_GET["request"] == "loadTFQue") {
+
+
+    //  echo "GET";
+  
+      $questionsPayload = loadTrueFalseQuestions($database);
+  
+      if ($questionsPayload) {
+          $length = count($questionsPayload);
+          $scratchQuestions = [];
+          if($length > 0) {
+              for ($i = 0; $i < $length; $i++) {
+  
+              
+                  $questions =
+                      array(
+                          "question_text" => $questionsPayload[$i]["QUESTION_TEXT"],
+                          "question_info" => $questionsPayload[$i]["QUESTION_INFORMATION"],
+                          "question_img" => $questionsPayload[$i]["QUESTION_IMAGE"],
+                          "tf_answer" => $questionsPayload[$i]["TF_ANSWER"]
+                      );
+                      array_push($trueFalseQuestions, $questions); 
+              }
+  
+          }
+    
+  
+  
+          $response['status'] = "Success";
+          $response['message'] = "Questions loaded!";
+          $response['trueFalseQuestions'] = $trueFalseQuestions;
+      } else {
+          $response['status'] = "Error";
+          $response['message'] = "Questions not found!"; 
+      }
+  
+  
+  
+  
+  
+  
+  
+    //  header("Content-type: application/json");
+      echo json_encode($response);
+  }
+  
+  
+  
+  function loadTrueFalseQuestions($database)
+  {
+      $sql = 'CALL `SELECT_TRUE_FALSE_QUESTIONS`()';
+      $sth = $database->pdo->prepare($sql);
+      $sth->execute();
+      return $sth->fetchAll(PDO::FETCH_SERIALIZE);
+  }
